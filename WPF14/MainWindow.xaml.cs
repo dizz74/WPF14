5e59ac1f6db0ff5125b6024bb02c7ef1b37e0c81
dizz74 <dizz74@mail.ru>
2021-12-06 13:23:34 +0500

    Add project files.

---
+++ WPF14/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            products = new ObservableCollection<Product>();
+            products.Add(new Product("Пирожок", 100, "Img/pie.png", ECategory.FOOD));
+            products.Add(new Product("Кофе машина", 4410, "Img/coffee-machine.png", ECategory.ELECTRONIC));
+            products.Add(new Product("Апельсин", 80, "Img/orange.png", ECategory.FOOD));
+            products.Add(new Product("Миксер", 1430, "Img/mixer.png", ECategory.ELECTRONIC));
+            products.Add(new Product("Рябина", 150, "Img/rowan.png", ECategory.FOOD));
+            products.Add(new Product("Духовка", 3850, "Img/oven.png", ECategory.ELECTRONIC));
+
+            listbox.ItemsSource = products;
+        }
+    }
+}
---
+++ WPF14/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF14
+{
+    public enum ECategory
+    {
+        FOOD,
+        ELECTRONIC
+    }
+    public class Product
+    {
+
+
+
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Pic { get; set; }
+        public ECategory Category { get; set; }
+
+        public Product(string name, int price, string pic, ECategory category)
+        {
+            Name = name;
+            Price = price;
+            Pic = pic;
+            Category = category;
+        }
+    }
+}
